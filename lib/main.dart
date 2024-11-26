995ef8b74562ac24c96722b4274644bb850e3075
youssef <ucifehab11@gmail.com>
2024-11-27 00:48:34 +0200

    refactor

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:news_app/application.dart';
 import 'package:news_app/application_view_model.dart';
 import 'package:provider/provider.dart';
 
-void main() async {
+void main() {
   runApp(
     ChangeNotifierProvider(
       create: (context) => ApplicationViewModel(),
